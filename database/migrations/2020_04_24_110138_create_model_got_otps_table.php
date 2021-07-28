358c50724605f3ebb284b79220099805e2f59c64
Irfaan Nujjoo <irfaan.nujjoo@telecom.mu>
2021-07-28 15:47:14 +0400

    [model][service][migration] use otp_generated_at column instead of created_at to check otp expiry

--- database/migrations/2020_04_24_110138_create_model_got_otps_table.php
+++ database/migrations/2020_04_24_110138_create_model_got_otps_table.php
@@ -18,6 +18,7 @@ class CreateModelGotOtpsTable extends Migration
             $table->string('model_id')->index('model_id_index');
             $table->string('model_type')->index('model_type_index');
             $table->string('otp_code');
+            $table->dateTime('otp_generated_at', $precision = 0);
             $table->boolean('otp_was_validated')->nullable();
             $table->timestamps();
         });
