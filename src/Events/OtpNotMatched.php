3ac4083635547a4edc2b0e7f2e363c798c453bda
Irfaan Nujjoo <irfaan.nujjoo@telecom.mu>
2022-03-11 15:30:06 +0400

    [events]
    events for otp expired, not found, not matched

---
+++ src/Events/OtpNotMatched.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace aliirfaan\LaravelSimpleOtp\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class OtpNotMatched
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Event name.
+     *
+     * @var string
+     */
+    public $name;
+
+    /**
+     * The user the attempter was trying to authenticate as.
+     *
+     * @var \Illuminate\Contracts\Auth\Authenticatable|null
+     */
+    public $user;
+
+    /**
+     * The credentials provided by the attempter.
+     *
+     * @var array
+     */
+    public $credentials;
+
+    /**
+     * Otp model
+     *
+     * @var aliirfaan\LaravelSimpleOtp\Models\ModelGotOtp|null
+     */
+    public $otpObj;
+
+    /**
+     * Create a new event instance.
+     * 
+     * @param  array  $credentials
+     * @param  \Illuminate\Contracts\Auth\Authenticatable|null $user
+     * @param  aliirfaan\LaravelSimpleOtp\Models\ModelGotOtp|null $otpObj
+     * @param  string  $name
+     * @return void
+     */
+    public function __construct($credentials, $user = null, $otpObj = null, $name = 'otp.not_match')
+    {
+        $this->user = $user;
+        $this->credentials = $credentials;
+        $this->name = $name;
+        $this->otpObj = $otpObj;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
